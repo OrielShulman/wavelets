8d56bdf90f5960a3afe532587fff267f5ba911f3
oriel <orielsh7@gmail.com>
2025-01-29 10:58:42 +0200

    added 2 plots figure

--- signal_display.py
+++ signal_display.py
@@ -1,19 +1,12 @@
-# import matplotlib
-# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def display_signals_together(signals: list[np.ndarray], titles: list[str]) -> None:
+def display_signals(signals: list[np.ndarray], titles: list[str]) -> None:
 	"""
 	Displays multiple 1D signals in the same figure with different colors and names in the legend.
-
-	Parameters:
-	-----------
-	signals : List[npt.NDArray[np.float64]]
-		A list of 1D signals to display.
-	titles : List[str]
-		A list of titles for the signals.
+	:param signals: list of 1D signals to display.
+	:param titles: list of titles for the signals.
 	"""
 	if len(signals) != len(titles):
 		raise ValueError("The number of signals and titles must be the same.")
@@ -29,4 +22,28 @@ def display_signals_together(signals: list[np.ndarray], titles: list[str]) -> No
 	plt.grid(True)
 	plt.title('1D Signals')
 	plt.show()
+
+
+def display_2_signals(signals: list[np.ndarray], titles: list[str]) -> None:
+	"""
+	display 2 signals in two diferent axis on the same figure.
+	:return:
+	"""
+	if len(signals) != len(titles) != 2:
+		raise ValueError("The number of signals and titles must be the 2.")
 	
+	fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex='all')
+	
+	ax1.plot(signals[0])
+	ax1.set_title(titles[0])
+	ax1.set_ylabel('Amplitude')
+	ax1.grid(True)
+	
+	ax2.plot(signals[1])
+	ax2.set_title(titles[1])
+	ax2.set_xlabel('Sample Index')
+	ax2.set_ylabel('Amplitude')
+	ax2.grid(True)
+	
+	plt.tight_layout()
+	plt.show()
