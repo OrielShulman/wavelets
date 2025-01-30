e66af0d1d3d42aa1849f691295f0673af64bbf0a
oriel <orielsh7@gmail.com>
2025-01-30 17:06:30 +0200

    added a method to compute the signal coefficients energy, also applied a method to compare the energy ofa signal. the DWT coeffs and the reconstructed signal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# from signal import Signal
-# import signal_display
+from signal import Signal
 import numpy as np
 import pywt
 
@@ -13,8 +12,9 @@ def simple_haar_transform(signal: np.ndarray) -> tuple[np.ndarray, np.ndarray, n
 	- details coefficients
 	- reconstructed signal
 	"""
+	# compute the wavelets coefficients: approximation and details
 	cA, cD = pywt.dwt(data=signal, wavelet='haar')
-	
+	# reconstruct the signal:
 	y = pywt.idwt(cA=cA, cD=cD, wavelet='haar')
 	
 	return cA, cD, y
@@ -27,19 +27,38 @@ def compute_dwt(signal: np.ndarray, wavelet: str = 'haar') -> tuple[np.ndarray, 
 	:param wavelet: wavelet method
 	:return: the approximation and details coefficients and a reconstructed signal.
 	"""
+	# apply decomposition to wavelet coefficients:
 	coefficients = pywt.wavedec(data=signal, wavelet=wavelet)
+	# reconstruct the signal:
 	reconstructed_signal = pywt.waverec(coeffs=coefficients, wavelet=wavelet)
+	
 	return coefficients, reconstructed_signal
 
 
+def compute_wavelet_coefficients_energy(coeffs: np.ndarray) -> float:
+	"""
+	computes the energy of a signals wavelet coefficients
+	:param coeffs: DWT coefficients.
+	:return: the energy of the signal coefficients.
+	"""
+	# flatten the coefficients and compute the total energy
+	coefficients_energy = Signal.compute_signal_energy(signal=np.concatenate(coeffs))
+	return coefficients_energy
+
+
 if __name__ == "__main__":
-	from signal import Signal
 	import signal_display as sd
 	
+	# TODO: implement Shifted Variant Wavelet Transform (with visualization).
+	# TODO: implement end to end simulator with results for SNR in dB.
+	# TODO: Read about Noise Suppression.
+	# TODO: Read about 'haar' Vs. 'Duvashi'.
+	# TODO: data - find good signals.
+	
 	# pulse signal parameters:
-	signal_size = 2 ** 8
-	pulse_width = 2 ** 6
-	pulse_start = (2 ** 5) - 1
+	signal_size = 2 ** 7
+	pulse_width = 2 ** 4
+	pulse_start = (2 ** 6)
 	pulse_amplitude = 10
 	# generate a pulse signal:
 	base_pulse_signal = Signal.pulse_1d(n_samples=signal_size,
@@ -56,10 +75,8 @@ if __name__ == "__main__":
 		:param signal: input signal to analyze
 		"""
 		cA, cD, y = simple_haar_transform(signal=signal)
-		sd.display_2_signals(signals=[cA,
-		                              cD],
-		                     titles=["Approximation coefficients",
-		                             "Detailed coefficients"])
+		sd.display_2_signals(signals=[cA, cD],
+		                     titles=["Approximation coefficients", "Detailed coefficients"])
 		sd.display_2_signals(signals=[signal,
 		                              y],
 		                     titles=["Signal",
@@ -82,8 +99,32 @@ if __name__ == "__main__":
 		# sd.display_2_signals(signals=[signal, signal - reconstructed_signal],
 		#                      titles=["Original Signal", "Original Signal - Reconstructed signal"])
 		
-	example_multy_level_haar_dwt(signal=base_pulse_signal)
+		return
+		
+	# example_multy_level_haar_dwt(signal=base_pulse_signal)
 	
-	# cA, cD = pywt.dwt(data=[3, 7, 1, 1, -2, 5, 4], wavelet='haar')
-	# y = pywt.idwt(cA=cA, cD=cD, wavelet='haar')
-	# exit(0)
+	def example_signal_energy(signal: np.ndarray) -> None:
+		"""
+		for a given signal, its wavelet decomposition and the reconstructed - compute the energies.
+		:param signal: input signal.
+		"""
+		# compute haar wavelet decomposition:
+		dwt_coefficients, reconstructed_signal = compute_dwt(signal=signal)
+		
+		# signal energy:
+		s_e = Signal.compute_signal_energy(signal=signal)
+		
+		# DWT coefficients energy:
+		c_e = compute_wavelet_coefficients_energy(coeffs=dwt_coefficients)
+		
+		# reconstructed signal energy:
+		rs_e = Signal.compute_signal_energy(signal=reconstructed_signal)
+		
+		print(f"signal energy: {s_e}\n"
+		      f"DWT coefficients energy: {c_e}\n"
+		      f"reconstructed signal energy: {rs_e}")
+		return
+	
+	# example_signal_energy(signal=base_pulse_signal)
+	
+	
