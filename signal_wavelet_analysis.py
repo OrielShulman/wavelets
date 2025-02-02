8461dc19edc2c065325819c7c4c8e27b9ae348dd
oriel <orielsh7@gmail.com>
2025-02-02 11:57:19 +0200

    initiates a class for handeling signal wavelet transform tools

---
+++ signal_wavelet_analysis.py
@@ -0,0 +1,33 @@
+import pywt
+import numpy as np
+from signal import Signal
+
+
+class SignalWaveletAnalysis:
+	"""
+	a class that performs on a given 1D signal wavelets analysis: decomposition, reconstruction, Etc.
+	"""
+	
+	def __init__(self, signal: np.ndarray):
+		"""
+		initiates the class for a given signal
+		:param signal: an input 1D signal
+		"""
+		self.signal = signal
+	
+	def one_level_wavelet_transform(self, wavelet: str = 'haar') -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+		"""
+		compute a single level depth wavelet transform
+		:param wavelet: wavelet method to use
+		:return: arrays of the:
+		- approximation coefficients
+		- details coefficients
+		- reconstructed signal
+		"""
+		# compute the wavelets coefficients: approximation and details
+		cA, cD = pywt.dwt(data=self.signal, wavelet=wavelet)
+		# reconstruct the signal:
+		y = pywt.idwt(cA=cA, cD=cD, wavelet='haar')
+		
+		return cA, cD, y
+		
