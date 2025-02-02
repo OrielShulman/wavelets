7193b34c97def554053c521642df3bea6a70e6d2
oriel <orielsh7@gmail.com>
2025-02-02 14:12:45 +0200

    moved all the methods out of __main__

--- main.py
+++ main.py
@@ -1,11 +1,134 @@
+import signal_display as sd
 from signal import Signal
+from signal_wavelet_analysis import SignalWaveletAnalysis
+
 import numpy as np
 import pywt
 
 
+def example_single_level_haar_dwt(signal: np.ndarray) -> None:
+	"""
+	execute visual example of a single level haar transform
+	:param signal: input signal to analyze
+	"""
+	cA, cD, y = SignalWaveletAnalysis(signal=signal).one_level_wavelet_transform()
+	sd.display_2_signals(signals=[cA, cD],
+	                     titles=["Approximation coefficients", "Detailed coefficients"])
+	sd.display_2_signals(signals=[signal,
+	                              y],
+	                     titles=["Signal",
+	                             "Reconstructed signal"])
+
+	
+def example_multy_level_haar_dwt(signal: np.ndarray, wavelet: str = 'haar', signal_info: str = None) -> None:
+	"""
+	execute visual example of a multy level haar DWT with visualization of coefficients scalogram
+	:param signal: input signal to analyze.
+	:param wavelet: wavelet method.
+	:param signal_info: signal information to attach with the plot title.
+	"""
+	# Apply DWT:
+	coeffs, reconstructed_signal = SignalWaveletAnalysis(signal=signal).compute_dwt(wavelet=wavelet)
+	
+	# Plot results scalogram:
+	sd.plot_dwt_scalogram(signal=signal, coeffs=coeffs, signal_info=signal_info, wavelet_method=wavelet)
+	
+	# Plot signal reconstruction inspection:
+	sd.display_2_signals(signals=[signal, np.abs(signal - reconstructed_signal)],
+	                     titles=["Original Signal", "| Original Signal - IDWT |"])
+	return
+
+
+def example_multy_level_haar_swt(signal: np.ndarray, wavelet: str = 'haar', signal_info: str = None) -> None:
+	"""
+	execute visual example of a haar SWT transform with visualization of coefficients scalogram
+	:param signal: input signal to analyze
+	:param wavelet: wavelet method.
+	:param signal_info: signal information to attach with the plot title.
+	"""
+	# Apply SWT:
+	coeffs, reconstructed_signal = SignalWaveletAnalysis(signal=signal).compute_swt(wavelet=wavelet)
+	
+	# Plot results scalogram:
+	sd.plot_swt_scalogram(signal=signal, coeffs=coeffs, signal_info=signal_info, wavelet_method=wavelet)
+	
+	# Plot signal reconstruction inspection:
+	sd.display_2_signals(signals=[signal, np.abs(signal - reconstructed_signal)],
+	                     titles=["Original Signal", "| Original Signal - ISWT |"])
+	return
+
+
+def example_compare_dwt_signal_energy(signal: np.ndarray, wavelet: str = 'haar') -> None:
+	"""
+	compare the signal evergy with its DWT coefficients and its IDWT energy.
+	:param signal: input signal.
+	:param wavelet: wavelet method.
+	"""
+	signal_analysis_mod = SignalWaveletAnalysis(signal=signal)
+	
+	# compute haar wavelet DWT decomposition:
+	dwt_coefficients, reconstructed_signal = signal_analysis_mod.compute_dwt(wavelet=wavelet)
+	
+	# signal energy:
+	s_e = Signal.compute_signal_energy(signal=signal)
+	
+	# DWT coefficients energy:
+	c_e = signal_analysis_mod.compute_dwt_coeffs_energy(coeffs=dwt_coefficients)
+	
+	# reconstructed signal energy:
+	rs_e = Signal.compute_signal_energy(signal=reconstructed_signal)
+	
+	print(f"signal energy: {s_e}\n"
+	      f"DWT coeffs energy: {c_e}\n"
+	      f"IDWT energy: {rs_e}")
+	return
+
+
+def example_haar_SWT(signal: np.ndarray) -> None:
+	"""
+	perform SWT and plot its coefficients along the signal (as plots)
+	"""
+	# Perform SWT
+	coeffs = pywt.swt(signal, 'haar')
+	n = len(coeffs)
+	# Plot the original signal and the SWT coefficients
+	plt.figure(figsize=(12, (1 + n) * 2))
+	plt.suptitle('SWT\n')
+	plt.subplot(n+1, 1, 1)
+	plt.plot(signal)
+	plt.title('Original Signal')
+	for i in range(n):
+		plt.subplot(n+1, 1, i + 2)
+		plt.plot(coeffs[i][0])
+		plt.title(f'Approximation Coefficients - Level {n - i}')
+	plt.tight_layout()
+	plt.show()
+	return
+
+
+def example_haar_DWT(signal: np.ndarray) -> None:
+	"""
+	perform DWT and plot its coefficients along the signal (as plots)
+	"""
+	# Perform DWT
+	coeffs = pywt.wavedec(signal, 'haar')
+	n = len(coeffs)
+	# Plot the original signal and the DWT coefficients
+	plt.figure(figsize=(12, n*2))
+	plt.suptitle('DWT\n')
+	plt.subplot(n, 1, 1)
+	plt.plot(signal)
+	plt.title('Original Signal')
+	for i in range(1, n):
+		plt.subplot(n, 1, i+1)
+		plt.plot(coeffs[i], marker='x', markerfacecolor='r', markeredgecolor='r')
+		plt.xticks([])
+		plt.title(f'Approximation Coefficients - Level {n - i}')
+	plt.tight_layout()
+	plt.show()
+
+
 if __name__ == "__main__":
-	import signal_display as sd
-	from signal_wavelet_analysis import SignalWaveletAnalysis as SWA
 	import matplotlib.pyplot as plt
 	
 	# TODO: implement end to end simulator with results for SNR in dB.
@@ -24,122 +147,22 @@ if __name__ == "__main__":
 	                                    amplitude=pulse_amplitude)
 	un_signal = Signal.add_uniform_noise_1d(signal=base_pulse_signal)
 	gn_signal = Signal.add_gaussian_noise_1d(signal=base_pulse_signal)
+	example_signal_s = np.sin(2 * np.pi * 7 * np.linspace(0, 1, 400)) + np.sin(2 * np.pi * 13 * np.linspace(0, 1, 400))
 	
+	# Execute examples:
 	
-	def example_haar_dwt(signal: np.ndarray) -> None:
-		"""
-		execute visual example of a single level haar transform
-		:param signal: input signal to analyze
-		"""
-		swt = SWA(signal=signal)
-		cA, cD, y = swt.one_level_wavelet_transform()
-		sd.display_2_signals(signals=[cA, cD],
-		                     titles=["Approximation coefficients", "Detailed coefficients"])
-		sd.display_2_signals(signals=[signal,
-		                              y],
-		                     titles=["Signal",
-		                             "Reconstructed signal"])
-	
-	# example_haar_dwt(signal=base_pulse_signal)
-	
-	
-	def example_multy_level_haar_dwt(signal: np.ndarray) -> None:
-		"""
-		execute visual example of a multy level haar transform with coefficients scalogram
-		:param signal: input signal to analyze
-		"""
-		coeffs, reconstructed_signal = SWA(signal=signal).compute_dwt(wavelet='haar')
-		signal_info = f"pulse signal [signal size: {signal_size}, " \
-		              f"pulse width: {pulse_width}, pulse start: {pulse_start}]"
-		sd.plot_dwt_scalogram(signal=signal, coeffs=coeffs, signal_info=signal_info, wavelet_method='haar')
-		
-		# signal reconstruction inspection:
-		# sd.display_2_signals(signals=[signal, signal - reconstructed_signal],
-		#                      titles=["Original Signal", "Original Signal - Reconstructed signal"])
-		
-		return
-	
-	# example_multy_level_haar_dwt(signal=base_pulse_signal)
-	
-	
-	def example_multy_level_haar_swt(signal: np.ndarray) -> None:
-		"""
-		execute visual example of a haar SWT transform with coefficients scalogram
-		:param signal: input signal to analyze
-		"""
-		coeffs, reconstructed_signal = SWA(signal=signal).compute_swt(wavelet='haar')
-		signal_info = f"pulse signal [signal size: {signal_size}, " \
-		              f"pulse width: {pulse_width}, pulse start: {pulse_start}]"
-		sd.plot_swt_scalogram(signal=signal, coeffs=coeffs, signal_info=signal_info, wavelet_method='haar')
-		
-		return
-	
-	# example_multy_level_haar_swt(signal=base_pulse_signal)
-	
-	def example_signal_energy(signal: np.ndarray) -> None:
-		"""
-		for a given signal, its wavelet decomposition and the reconstructed - compute the energies.
-		:param signal: input signal.
-		"""
-		# compute haar wavelet DWT decomposition:
-		dwt_coefficients, reconstructed_signal = SWA(signal=signal).compute_dwt(wavelet='haar')
-		
-		# # compute haar wavelet SWT decomposition:
-		# swt_coefficients, reconstructed_signal = compute_dwt(signal=signal)
-		
-		# signal energy:
-		s_e = Signal.compute_signal_energy(signal=signal)
-		
-		# DWT coefficients energy:
-		c_e = SWA(signal=signal).compute_dwt_coeffs_energy(coeffs=dwt_coefficients)
-		
-		# reconstructed signal energy:
-		rs_e = Signal.compute_signal_energy(signal=reconstructed_signal)
-		
-		print(f"signal energy: {s_e}\n"
-		      f"DWT coeffs energy: {c_e}\n"
-		      f"IDWT energy: {rs_e}")
-		return
-	
-	example_signal_energy(signal=base_pulse_signal)
-	
-	
-	def example_shift_invariant_wavelet_transform(signal: np.ndarray):
-		# Perform SWT
-		coeffs = pywt.swt(signal, 'haar', level=4)
-		# Plot the original signal and the SWT coefficients
-		plt.figure(figsize=(12, 8))
-		plt.suptitle('Shift invariant')
-		plt.subplot(5, 1, 1)
-		plt.plot(signal)
-		plt.title('Original Signal')
-		for i in range(4):
-			plt.subplot(5, 1, i + 2)
-			plt.plot(coeffs[i][0])
-			plt.title(f'SWT Approximation Coefficients - Level {i + 1}')
-		plt.tight_layout()
-		plt.show()
-		return
-	
-	def example_DWT(signal: np.ndarray):
-		# Perform DWT
-		coeffs = pywt.wavedec(signal, 'haar', level=4)
-		# Plot the original signal and the DWT coefficients
-		plt.figure(figsize=(12, 8))
-		plt.suptitle('DWT')
-		plt.subplot(5, 1, 1)
-		plt.plot(signal)
-		plt.title('Original Signal')
-		for i in range(1, 5):
-			plt.subplot(5, 1, i + 1)
-			plt.plot(coeffs[i])
-			plt.title(f'DWT Coefficients - Level {i}')
-		plt.tight_layout()
-		plt.show()
-	
-	
-	# # Generate a sample signal
-	# example_signal_s = np.sin(2 * np.pi * 7 * np.linspace(0, 1, 400)) + np.sin(2 * np.pi * 13 * np.linspace(0, 1, 400))
-	
-	# example_shift_invariant_wavelet_transform(signal=base_pulse_signal)
-	# example_DWT(signal=base_pulse_signal)
+	# example_single_level_haar_dwt(signal=base_pulse_signal)
+	#
+	# example_multy_level_haar_dwt(signal=base_pulse_signal,
+	#                              signal_info=f"pulse signal [signal size: {signal_size}, "
+	#                                          f"pulse width: {pulse_width}, pulse start: {pulse_start}]")
+	#
+	# example_multy_level_haar_swt(signal=base_pulse_signal,
+	#                              signal_info=f"pulse signal [signal size: {signal_size}, "
+	#                                          f"pulse width: {pulse_width}, pulse start: {pulse_start}]")
+	#
+	# example_compare_dwt_signal_energy(signal=base_pulse_signal)
+	#
+	# example_haar_SWT(signal=base_pulse_signal)
+	#
+	# example_haar_DWT(signal=base_pulse_signal)
