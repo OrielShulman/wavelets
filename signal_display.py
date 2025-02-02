d9a0bc4812646f7532fa0a73e5968480b2af3e42
oriel <orielsh7@gmail.com>
2025-02-02 18:02:41 +0200

    added logrithmic scale view for the SWT

--- signal_display.py
+++ signal_display.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def display_signals(signals: list[np.ndarray], titles: list[str]) -> None:
@@ -131,53 +132,86 @@ def plot_swt_scalogram(signal: np.ndarray,
 	:param signal_info: signal information to attach with the title.
 	:param wavelet_method: wavelet method used for SWT
 	"""
-	# Create a figure with 2 subplots
-	fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 12), sharex='all', gridspec_kw={'height_ratios': [2, 5, 5]})
+	# Create a figure with subplots
+	fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, figsize=(10, 16), sharex='all',
+	                                         gridspec_kw={'height_ratios': [3, 5, 5, 5]})
 	
 	# Set the main title
 	fig.suptitle(f"SWT scalogram for:\n\n{signal_info}\n", fontweight='bold')
 	
-	# Plot the original signal
+	# ax1: Plot the original signal
 	ax1.plot(signal)
 	ax1.set_title(f"Original Signal ({signal.size} samples)")
 	ax1.set_xlabel('Sample')
 	ax1.set_ylabel('Amplitude')
 	ax1.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True)
 	
-	# Plot the scalogram:
-	# Combine all detail coefficients for visualization
-	combined_coeffs = np.vstack([c[1] for c in coeffs])
-	cax = ax2.imshow(combined_coeffs,
-	                 extent=[0, len(signal), 0.5, len(coeffs) + 0.5],
-	                 aspect='auto',
-	                 cmap='coolwarm',
-	                 vmin=np.min(combined_coeffs),
-	                 vmax=np.max(combined_coeffs))
-	# set scalogram ticks:
+	# ax2: Plot the scalogram:
+	combined_coeffs = np.vstack([c[1] for c in coeffs])  # Combine all detail coefficients for visualization
+	cax2 = ax2.imshow(combined_coeffs,
+	                  extent=[0, len(signal), 0.5, len(coeffs) + 0.5],
+	                  aspect='auto',
+	                  cmap='coolwarm',
+	                  vmin=np.min(combined_coeffs),
+	                  vmax=np.max(combined_coeffs))
 	ax2.set_yticks(range(1, len(combined_coeffs) + 1))
 	ax2.set_yticklabels([f'{i}' for i in range(1, len(combined_coeffs) + 1)])
 	ax2.set_xlabel('Sample')
 	ax2.set_ylabel('Level')
 	ax2.set_title(f"Scalogram of SWT Detail Coefficients ({wavelet_method})")
-	fig.colorbar(cax, ax=ax2, orientation='horizontal', label='Coefficient Value')
+	fig.colorbar(cax2, ax=ax2, orientation='horizontal', label='Coefficient Value')
 	
-	# Plot the abs value scalogram:
-	# Combine all detail coefficients for visualization
-	abs_combined_coeffs = np.vstack([np.abs(c[1]) for c in coeffs])
-	abs_cax = ax3.imshow(abs_combined_coeffs,
-	                     extent=[0, len(signal), 0.5, len(coeffs) + 0.5],
-	                     aspect='auto',
-	                     cmap='Spectral',
-	                     vmin=np.min(abs_combined_coeffs),
-	                     vmax=np.max(abs_combined_coeffs))
-	# set abs_scalogram ticks:
+	# ax3: Plot the abs value scalogram:
+	abs_combined_coeffs = np.abs(combined_coeffs)
+	cax3 = ax3.imshow(abs_combined_coeffs,
+	                  extent=[0, len(signal), 0.5, len(coeffs) + 0.5],
+	                  aspect='auto',
+	                  cmap='Spectral',
+	                  vmin=np.min(abs_combined_coeffs),
+	                  vmax=np.max(abs_combined_coeffs))
 	ax3.set_yticks(range(1, len(abs_combined_coeffs) + 1))
 	ax3.set_yticklabels([f'{i}' for i in range(1, len(abs_combined_coeffs) + 1)])
 	ax3.set_xlabel('Sample')
 	ax3.set_ylabel('Level')
 	ax3.set_title(f"Absolute value Scalogram of SWT Detail Coefficients ({wavelet_method})")
-	fig.colorbar(abs_cax, ax=ax3, orientation='horizontal', label='Coefficient Value')
-	# Add colorbar at the scalogram bottom
+	fig.colorbar(cax3, ax=ax3, orientation='horizontal', label='Coefficient Value')
 	
+	# ax4: Plot the scalogram in dB:
+	db_combined_coeffs = 20 * np.log10(abs_combined_coeffs + np.finfo(float).eps)
+	cax4 = ax4.imshow(db_combined_coeffs,
+	                  extent=[0, len(signal), 0.5, len(coeffs) + 0.5],
+	                  aspect='auto',
+	                  cmap='magma',
+	                  vmin=np.min(db_combined_coeffs),
+	                  vmax=np.max(db_combined_coeffs))
+	ax4.set_yticks(range(1, len(db_combined_coeffs) + 1))
+	ax4.set_yticklabels([f'{i}' for i in range(1, len(db_combined_coeffs) + 1)])
+	ax4.set_xlabel('Sample')
+	ax4.set_ylabel('Level')
+	ax4.set_title(f"logarithmic Scalogram of SWT Detail Coefficients [dB] ({wavelet_method})")
+	fig.colorbar(cax4, ax=ax4, orientation='horizontal', label='Coefficient Value')
+	
+	plt.tight_layout()
+	plt.show()
+
+
+def plod_pandas_df(df: pd.DataFrame, plot_title: str) -> None:
+	"""
+	plots a given pandas df
+	:param df: signal dataframe
+	:param plot_title: title
+	"""
+	# Plot each column in the DataFrame
+	plt.figure(figsize=(12, 6))
+	plt.suptitle(plot_title, fontweight='bold')
+	for column in df.columns:
+		plt.plot(df.index, df[column], label=column)
+	
+	# Add legend, title, and labels
+	plt.legend()
+	plt.title('Signals')
+	plt.xlabel('Sample Index')
+	# plt.ylabel('Signal Value')
+	plt.grid(True)
 	plt.tight_layout()
 	plt.show()
